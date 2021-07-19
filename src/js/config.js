0951cf9bc70f7d534b25f6083cafc1da481039b8
Angry Wasp <angrywasp@live.com>
2021-07-19 12:36:12 +1000

    Add build directory to repo for easier deployment

--- src/js/config.js
+++ src/js/config.js
@@ -11,7 +11,7 @@ export class Config {
 
     static app = {
 
-        cpNet: "testnet",
+        cpNet: "mainnet",
         serverCount: 3,
         serverDomain: "civiport.online",
         confTime: 12,
